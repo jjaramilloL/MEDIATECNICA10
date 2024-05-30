bc46a8e3a6f20e04962bc25ee5e665e629779b74
jjaramilloL <jaramilloliset12@gmail.com>
2024-05-30 16:37:25 -0500

    Create aprendisaje de swich.cs

---
+++ aprendisaje de swich.cs
@@ -0,0 +1,34 @@
+/******************************************************************************
+AUTOR:liset jaramillo
+FECHA:30-05-2024
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+      Console.WriteLine("digite la nota que a obtenido (1-5) ");
+      Console.WriteLine("opcion");
+    int opcion = Int32.Parse(Console.ReadLine());
+      switch (opcion)
+     {
+  case 1:
+     Console.WriteLine("pierde");
+     break;
+  case 2:
+     Console.WriteLine("pierde");
+     break;
+  case 3:
+     Console.WriteLine("gana");
+     break;
+  case 4:
+     Console.WriteLine("alto");
+     break;
+  case 5:
+     Console.WriteLine("superior");
+     break;
+     default;
+      console.writeLne("opcion no valida");
+      console.writeLne("saliendo del programa... ");
+     break;
+    } 
+  }
+}
